e6f3347ad3b85b769b2702569abb0139534082aa
Really Simple <andy@reallysimpleworks.com>
2013-07-24 15:39:02 +0100

    Added error reporting.

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -4,6 +4,8 @@
 	
 	<div class="home-page">
 		
+		<section class="home-page-section">
+		
 				<h2>Wildtime</h2>
 				
 				<p>We love technology but there are amazing connections to be found outside in nature. So swap some screen time for some Wild Time.</p>
@@ -11,14 +13,18 @@
 				<p>Good luck.</p>
 
 				<p><a href="https://itunes.apple.com/gb/app/wild-time/id658430256?mt=8&uo=4" target="itunes_store"><img src="http://r.mzstatic.com/images/web/linkmaker/badge_appstore-lrg.gif" alt="Wild Time - Fieldwork" style="border: 0;"/></a></p>
+				
+		</section>
+		
+		<section class="home-page-section">
 
-				<h2>Project Wild Thing</h2>
+			<h2>Project Wild Thing</h2>
+			
+			<p>Project Wild Thing is a film led movement to get more kids playing freely outside and re-connecting with the natural world around them.</p>
+			<p>Find out more and watch the film at <a href="http://projectwildthing.com">projectwildthing.com</a></p>
+			<p>We are powered by The Wild Network a growing network of charities and organisations committed to tackling the issues raised in the film. <a href="http://thewildnetwork.com">thewildnetwork.com</a></p>
 				
-				<p>Project Wild Thing is a film led movement to get more kids playing freely outside and re-connecting with the natural world around them.</p>
-				<p>Find out more and watch the film at <a href="http://projectwildthing.com">projectwildthing.com</a></p>
-				<p>We are powered by The Wild Network a growing network of charities and organisations committed to tackling the issues raised in the film. <a href="http://thewildnetwork.com">thewildnetwork.com</a></p>
-				
-				
+		</section>
 				
 					
 				
